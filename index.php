8f0172f422fa0e91f277c27eb1ef10577d295156
thiodice <thiodice@proton.me>
2023-07-05 20:33:58 +0200

    Scalable font size

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Dusky Peak</title>
   <link rel="stylesheet" href="/styles/reset.css">
+  <link rel="stylesheet" href="/styles/main.css">
 </head>
 <body>
   <?php include('./components/page-navigation.inc.php') ?>
